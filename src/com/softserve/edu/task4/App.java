65dc68f95592d99bb627079f98a36d1bd31b7970
khdimon <khdimon@gmail.com>
2017-05-25 08:09:13 +0300

    Task4 with javadoc comments

--- src/com/softserve/edu/task4/App.java
+++ src/com/softserve/edu/task4/App.java
@@ -1,15 +1,28 @@
+
 package com.softserve.edu.task4;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 /**
- * Created by Dmitrij on 24.05.17.
+ * Solve task4.
  */
 public class App {
-    public String readFile(String path) throws IOException {
+    /**
+     * Reads file to string and returns it.
+     * @param path the path to the file
+     * @return result string
+     * @throws IOException If an I/O error occurs
+     */
+    public String readFile(final String path) throws IOException {
         File file = new File(path);
         BufferedReader reader = new BufferedReader(new FileReader(file));
         String string = "";
+        @SuppressWarnings("CheckStyle")
         char[] buffer = new char[100];
         int symbols;
         while (reader.ready()) {
@@ -21,7 +34,13 @@ public class App {
         return string;
     }
 
-    public int countOccurrences(String source, String target) {
+    /**
+     * Counts occurrences of the target string in given string.
+     * @param source string that we seek occurrences of target string in
+     * @param target string that occurrences we seek
+     * @return count of occurrences
+     */
+    public int countOccurrences(final String source, final String target) {
         int number = 0;
         int index = 0;
         while (true) {
@@ -36,7 +55,13 @@ public class App {
         return number;
     }
 
-    public void writeFile(String path, String content) throws IOException {
+    /**
+     * Writes string into the file.
+     * @param path path to the file
+     * @param content given string
+     * @throws IOException If an I/O error occurs
+     */
+    public void writeFile(final String path, final String content) throws IOException {
         File file = new File(path);
         BufferedWriter writer = new BufferedWriter(new FileWriter(file));
         writer.write(content);
@@ -44,27 +69,36 @@ public class App {
         writer.close();
     }
 
-    public static void main(String[] args) throws IOException {
-        if (args.length < 2) {
-            System.out.println("Enter: \"<Path to the file> <target string>\" to count occurrences"
-                    + "\nor \"<Path to the file> <target string> <string for replacement>\" to replacement");
-            return;
-        }
-
-        App app = new App();
-        String content = app.readFile(args[0]);
-
-        if (args.length == 2) {
-            System.out.print("Number of occurrences: ");
-            System.out.println(app.countOccurrences(content, args[1]));
-        } else {
-            if (!content.contains(args[1])) {
-                System.out.println("This string is absent in the file.");
-            } else {
-                content = content.replace(args[1], args[2]);
-                app.writeFile(args[0], content);
-                System.out.println("All occurrences of this string has been replaced.");
+    /**
+     * Starts the program.
+     * @param args parameters of the command line
+     */
+    @SuppressWarnings("CheckStyle")
+    public static void main(String[] args) {
+        try {
+            if (args.length < 2) {
+                System.out.println("Enter: \"<Path to the file> <target string>\" to count occurrences"
+                        + "\nor \"<Path to the file> <target string> <string for replacement>\" to replacement");
+                return;
             }
+
+            App app = new App();
+            String content = app.readFile(args[0]);
+
+            if (args.length == 2) {
+                System.out.printf("Count of occurrences of string \"%s\": ", args[1]);
+                System.out.println(app.countOccurrences(content, args[1]));
+            } else {
+                if (!content.contains(args[1])) {
+                    System.out.printf("The string \"%s\" is absent in the file.%n", args[1]);
+                } else {
+                    content = content.replace(args[1], args[2]);
+                    app.writeFile(args[0], content);
+                    System.out.printf("All occurrences of string \"%s\" have been replaced on \"%s\".%n", args[1], args[2]);
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Some problems with input or output have occurred.");;
         }
     }
 }
